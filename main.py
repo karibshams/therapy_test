3e4c862f7f51651772fea7c6947b0a2d2adac47d
karib shams <154616482+karibshams@users.noreply.github.com>
2025-08-19 22:29:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ class EmothriveAI:
             response = self.client.chat.completions.create(
                 model=self.model,
                 messages=messages,
-                max_tokens=300
+                max_tokens=450
             )
             response_text = response.choices[0].message.content
 
